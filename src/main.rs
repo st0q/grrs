196c4fabbe8779a46b8ac00870f5f268da390be6
Tatsuya Sasaki <tssk713@gmail.com>
2024-05-27 23:22:28 +0900

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use std::io::BufRead;
-use anyhow::{Context, Result};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -18,7 +18,10 @@ fn main() -> Result<()> {
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
     let reader = std::io::BufReader::new(file);
-    let content = reader.lines().map(|line| line.unwrap()).collect::<Vec<String>>();
+    let content = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
 
     for line in &content {
         if line.contains(&args.pattern) {
